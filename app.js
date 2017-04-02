a46de79de4318ab931c93dd19744bd4f73a911a8
ASWANTH K <aswanthkoleri@users.noreply.github.com>
2017-04-02 23:27:00 +0530

    Inital Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+        $("#results").html('<img src="loader.gif" alt="Generating Song "/>');
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 10,
+            order: "viewCount"
+       });
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("button.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+});
+
+function init() {
+    gapi.client.setApiKey("AIzaSyBfsRf40RP0PU0InG3Pd7okMwGW0OkpXQg"); 
+    gapi.client.load("youtube", "v3", function() {
+    });
+}
